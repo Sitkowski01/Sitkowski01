1fcb166bfeb5afff20a10db14677e291dff79dbe
Sitkowski01 <113860600+Sitkowski01@users.noreply.github.com>
2022-09-26 23:55:59 +0200

    Add files via upload

---
+++ learningJava/areaCalculator.java
@@ -0,0 +1,22 @@
+public class areaCalculator {
+    public static void main(String args[]){
+    double circle = area(5.0);
+        System.out.println(circle);
+        double rectangle = area(5.0,4.0);
+        System.out.println(rectangle);
+    }
+    public static double area(double radius){
+        if(radius < 0)
+            return -1;
+
+        double circle = radius * radius * Math.PI;
+        return circle;
+    }
+    public static double area(double x,double y){
+        if(x < 0 || y < 0)
+            return -1;
+
+        double rectangle = x * y;
+        return rectangle;
+    }
+}
